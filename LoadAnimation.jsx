6c1c9a48069d8330e80cdabf9bc5478c328a36da
Tommy Pettersson <tommypet@kth.se>
2019-04-07 16:50:08 +0200

    initial commit

---
+++ LoadAnimation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LoadAnimation.css';
+
+const LoadAnimation = ({element}) => 
+{
+    return (
+        <div className="loadAnimation">{element}</div>
+    )
+};
+
+export default LoadAnimation;
